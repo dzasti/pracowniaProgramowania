237c4b63da19587885e3e81400c7a89767e5aa88
Justyna Wojsz <just.justa7@gmail.com>
2020-11-20 14:59:14 +0100

    1 widok jako strona html

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('home')
+	return render(request,'accounts/strona.html')
 
 def products(request):
 	return HttpResponse('products')
